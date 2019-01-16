ba7dc2e9b72535c2392710bc0798f2d406fc43c4
Radovan <studio@pythonforce.com>
2019-01-16 13:40:48 -0500

    making the season longer

--- menu/models.py
+++ menu/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class Menu(models.Model):
-    season = models.CharField(max_length=50)
+    season = models.CharField(max_length=200)
     items = models.ManyToManyField('Item', related_name='items')
     created_date = models.DateTimeField(
             default=timezone.now)
