aa40896272e12321759327428fbeed4e3b4af334
Radovan <studio@pythonforce.com>
2019-01-16 14:06:03 -0500

    items

--- menu/urls.py
+++ menu/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('', views.menu_list, name='menu_list'),
     path('<int:pk>/edit/', views.edit_menu, name='menu_edit'),
     path('<int:pk>/', views.menu_detail, name='menu_detail'),
-    path('item/<int:pk>/edit', views.edit_item, name='item_edit'),
-    path('item/<int:pk>/', views.item_detail, name='item_detail'),
-    path('item/', views.item_list, name='item_list'),
+    path('items/<int:pk>/edit', views.edit_item, name='item_edit'),
+    path('items/<int:pk>/', views.item_detail, name='item_detail'),
+    path('items/', views.item_list, name='item_list'),
     path('new/', views.create_new_menu, name='menu_new'),
 ]
